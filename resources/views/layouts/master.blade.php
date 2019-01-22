d8b9ad8577f032e532d6b15a9d4154babadc5d64
wesinalves <wesinec@gmail.com>
2019-01-22 16:04:07 -0200

    edit admin template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -61,7 +61,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="./img/profile.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
       </div>
 
@@ -70,11 +70,22 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
+          
+         <li class="nav-item">
+           <a href="#" class="nav-link">
+             <i class="nav-icon fa fa-th"></i>
+             <p>
+               dashboard
+             </p>
+           </a>
+         </li>
+
+
+          <li class="nav-item has-treeview">
             <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon fas fa-cog"></i>
               <p>
-                Starter Pages
+                Management
                 <i class="right fa fa-angle-left"></i>
               </p>
             </a>
@@ -94,14 +105,23 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </ul>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
-          </li>
+           <a href="#" class="nav-link">
+             <i class="nav-icon fa fa-user"></i>
+             <p>
+               profile
+             </p>
+           </a>
+         </li>
+
+         <li class="nav-item">
+           <a href="#" class="nav-link">
+             <i class="nav-icon fa fa-power-off"></i>
+             <p>
+               logout
+             </p>
+           </a>
+         </li>
+
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
