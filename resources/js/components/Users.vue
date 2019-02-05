e1779b620645edd03d3c8546b71925a3553a9965
wesinalves <wesinec@gmail.com>
2019-02-05 16:49:25 -0200

    load user with vuejs

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -20,13 +20,15 @@
                             <th>Name</th>
                             <th>Email</th>
                             <th>Type</th>
+                            <th>Registered at</th>
                             <th>Modify</th>
                           </tr>
-                          <tr>
-                            <td>183</td>
-                            <td>John Doe</td>
-                            <td>11-7-2014</td>
-                            <td><span class="tag tag-success">Approved</span></td>
+                          <tr v-for="user in users" :key="user.id">
+                            <td>{{user.id}}</td>
+                            <td>{{user.name}}</td>
+                            <td>{{user.email}}</td>
+                            <td>{{user.type}}</td>
+                            <td>{{user.created_at}}</td>
                             <td>
                                 <a><i class='fa fa-edit blue'></i>
                                 </a> /
@@ -107,6 +109,7 @@
     export default {
         data(){
           return{
+            users: {}, 
             form: new Form({
               name: '',
               email: '',
@@ -118,12 +121,15 @@
           }
         },
         methods: {
+            loadUsers(){
+              axios.get("api/user").then(({ data }) => (this.users = data.data));
+            },
             createUser(){
               this.form.post('api/user');
             }
         },
-        mounted() {
-            console.log('Component mounted.')
+        created() {
+            this.loadUsers();
         }
     }
 </script>
